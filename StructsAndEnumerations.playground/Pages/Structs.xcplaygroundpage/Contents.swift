06480211be1d1a04a185c54ec8c064857cc4d961
Johnny Hicks <johnnydhicks@gmail.com>
2019-03-14 23:35:56 -0600

    Create structs and enums assignment

---
+++ StructsAndEnumerations.playground/Pages/Structs.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+/*:
+ ## Structs
+ Create a struct type for an iOS Engineer. Your type should contain properties for name, company, years of experience, salary, programming language, and an isEligibleForBenefits bool property.
+ */
+
+/*:
+ Create an instance of your iOSEngineer struct using your future engineer self. Print out a statement that includes the salary of your instance (ex. "When I am an engineer, I will be making $[salary].")
+ */
+
+
+/*:
+ Add a custom initializer to your struct giving the programming language property a default value of "Swift".
+ */
+
+//: page 1 of 2 | [Next: Enumerations](@next)
