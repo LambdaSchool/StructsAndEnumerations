aad9b0ca7cbb8b0a50035d328290e0b613647799
Johnny Hicks <johnnydhicks@gmail.com>
2019-03-14 23:38:17 -0600

    Add page numbers

--- StructsAndEnumerations.playground/Pages/Enumerations.xcplaygroundpage/Contents.swift
+++ StructsAndEnumerations.playground/Pages/Enumerations.xcplaygroundpage/Contents.swift
@@ -4,3 +4,5 @@
  Create an enum for the current iPhone product line. Copy and paste your struct from the last page into this page. Add one more property to your iOS Engineer struct of type iPhone. Create an instance of iOS Engineer, that contains your new iPhone property.
  */
 
+//: page 2 of 2
+
