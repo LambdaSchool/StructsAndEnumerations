5bc6446678fa6711add50474b3e96feebb269397
Spencer Curtis <spencercolecurtis@gmail.com>
2019-08-23 15:04:57 -0600

    Fix minor typos

--- StructsAndEnumerations.playground/Pages/Structs.xcplaygroundpage/Contents.swift
+++ StructsAndEnumerations.playground/Pages/Structs.xcplaygroundpage/Contents.swift
@@ -1,15 +1,15 @@
 /*:
  ## Structs
- Create a struct type for an iOS Developer. Your type should contain properties for name, company, years of experience, salary, programming language, and an isEligibleForBenefits bool property.
+ Create a struct type for an iOS Developer called `iOSDeveloper`. Your type should contain properties for name, company, years of experience, salary, programming language, and and an `isEligibleForBenefits` property that should be a `Bool`.
  */
 
 /*:
- Create an instance of your iOSDeveloper struct using your future developer self. Print out a statement that includes the salary of your instance (ex. "When I am a developer, I will be making $[salary].")
+ Create an instance of your `iOSDeveloper` struct using your future developer self. Print out a statement that includes the salary of your instance (ex. "When I am a developer, I will be making $[salary].")
  */
 
 
 /*:
- Add a custom initializer to your struct giving the programming language property a default value of "Swift".
+ Go back to the struct and add a custom initializer to it, giving the programming language property a default value of "Swift".
  */
 
 //: page 1 of 2 | [Next: Enumerations](@next)
